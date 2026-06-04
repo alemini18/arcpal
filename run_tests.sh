914ac7eba734a63ddcd80359de8ba2e614d31bf5
Alessandro Minisini <aleminipublic@gmail.com>
2026-06-04 10:28:51 +0200

    adding csv stats

--- run_tests.sh
+++ run_tests.sh
@@ -20,8 +20,8 @@ TOTAL=0
 python3 compact_to_dimacs.py "$2"
 
 # 1. Definisci i percorsi dei CSV globali e rimuovi i vecchi di run precedenti
-GLOBAL_KERN_CSV="tests/sudoku/output/nsys_kernels_summary.csv"
-GLOBAL_MEM_CSV="tests/sudoku/output/nsys_memory_summary.csv"
+GLOBAL_KERN_CSV="tests/sudoku/output/{$EXEC}_nsys_kernels_summary.csv"
+GLOBAL_MEM_CSV="tests/sudoku/output/{$EXEC}_nsys_memory_summary.csv"
 rm -f "$GLOBAL_KERN_CSV" "$GLOBAL_MEM_CSV"
 
 # Loop through all .in files in the test directory

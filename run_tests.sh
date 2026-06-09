8387c3cc37da561303d35702344852deb9f91b8d
Alessandro Minisini <aleminipublic@gmail.com>
2026-06-09 14:07:18 +0200

    more perf

--- run_tests.sh
+++ run_tests.sh
@@ -35,8 +35,8 @@ for test_file in tests/sudoku/input/*.in; do
     STATS_CSV="tests/sudoku/output/${filename}_nsys_stats.csv"
     
     nsys profile -t cuda --force-overwrite=true -o "$NSYS_REP" "$EXEC" < "$test_file" > "$TMP_OUT"
-    nsys stats --force-report=true "${NSYS_REP}.nsys-rep" >> "$STATS_LOG"
-    nsys stats --report=cuda_gpu_kern_sum --force-export=true --format=csv "${NSYS_REP}.nsys-rep" > "$STATS_CSV"
+    nsys stats --force-export=true "${NSYS_REP}.nsys-rep" >> "$STATS_LOG"
+    nsys stats --report=cuda_gpu_kern_sum,cuda_gpu_mem_size_sum,cuda_api_sum --force-export=true --format=csv "${NSYS_REP}.nsys-rep" > "$STATS_CSV"
     
     if [ -s "$STATS_CSV" ]; then
         awk -v f="$filename" -F',' '$1 ~ /^[0-9]+\.?[0-9]*$/ {print f "," $0}' "$STATS_CSV" >> "$GLOBAL_CSV"
